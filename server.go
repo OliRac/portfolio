bd5334e99bc0a6a2109df6ce1191efaa83f913d2
OliRac <olivieracette@gmail.com>
2021-01-20 15:15:22 -0500

    using ServeFile
    Its another way of doing it. Idk if its efficient (are files loaded from disk with every request?), but still this is such a small project that it probably doesn't matter.

--- server.go
+++ server.go
@@ -4,67 +4,39 @@ import (
 	"fmt"
 	"net/http"
 	"io/ioutil"
+	"path/filepath"
 )
 
-func homeHandler(resp http.ResponseWriter, req *http.Request){
-
-	//since the pattern is just a slash, just checking if theres anything else in there
-	//Entering junk along with slash doesnt give 404 errors
+//Also handles 404 if anything is after /
+//Servefile does some sanitizing already
+//home is unforunately a global. Will try to come up with a way to not use globals.
+func homeHandler(res http.ResponseWriter, req *http.Request){
 	if req.URL.Path != "/" {
-		//errorHandler(resp, req, http.StatusNotFound)
-		
-		resp.WriteHeader(http.StatusNotFound)
-		fmt.Fprintf(resp, "404 not found")		//custom 404 could go here
 
-        return
-    }
+		res.WriteHeader(http.StatusNotFound)
+		fmt.Fprintf(res, "404 not found")		//custom 404 could go here
 
-	fmt.Fprintf(resp, home)
-}
-
-func glslHandler(resp http.ResponseWriter, req *http.Request){
-	fmt.Fprintf(resp, "to do")
-}
-
-//Helper for errors
-func checkErr(e error){
-	if e!= nil {
-		panic(e)
+		return
 	}
+
+	http.ServeFile(res, req, home)
 }
 
-//Loads a text file in memory
-//Panic on error
-func loadFile(path string) string {
-	data, err := ioutil.ReadFile(path)
-
-	checkErr(err)
-
-	return string(data)
+func glslHandler(res http.ResponseWriter, req *http.Request){
+	fmt.Fprintf(res, "to do")
 }
 
-//Pages to load in memory
-var home = loadFile("./static/sample.html")
-//var glsl = loadFile("...")
+
+//Files to serve
+var home = filepath.FromSlash("./static/sample.html")
+//var glsl = filepath.FromSlash("./static/glsl.html")
 
 func main() {
 	fmt.Println("Starting up server...")
 
-	//Load up the pages to server memory
-	//page := loadFile("./static/sample.html")
-	//fmt.Println(page)
-
-	//staticDir := "./static/sample.html"
-	//fs := http.FileServer(http.Dir(staticDir))
-
-	//Using the default Mux (this is a simple project anyway, it will suffice)
-	/*http.HandleFunc("/", func(res http.ResponseWriter, req * http.Request){
-		http.ServeFile(res, req, staticDir)
-	})*/
-
+	//init and launch
 	http.HandleFunc("/", homeHandler)
 	http.HandleFunc("/glsl", glslHandler)
 
-	//Create & launch server
 	http.ListenAndServe(":8080", nil)
 }

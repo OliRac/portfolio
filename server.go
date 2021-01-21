9f8dba653b5e2a219d2da9b32c9fc9c2e4c4be8f
OliRac <olivieracette@gmail.com>
2021-01-21 13:47:01 -0500

    html templates
    Had trouble starting this off because I forgot an {{end}} tag. Tl;dr if you get nil pointer from Execute() or ExecuteTemplates, assume it's because you have some syntax errors in your templates.

--- server.go
+++ server.go
@@ -4,23 +4,43 @@ import (
 	"fmt"
 	"net/http"
 	"path/filepath"
+	"html/template"
 )
 
 
 //Files & directories to serve, port to use
 //Assuming globals use PascalCase in Go to differentiate from locals
 var (
-	StaticDir = filepath.FromSlash("./static")
+	StaticDir = filepath.FromSlash("./static/")
+	TemplateDir = filepath.FromSlash("./templates/")
 	Port = "8080"
 )
 
+//A dummy sample. Will simply serve the same page as static/index.html but split off in template sections
+//Doing this to learn go html/templates
+//Going step by step! slowly but surely.
+func serveSample(res http.ResponseWriter, req *http.Request) {
+	tmpl, _ := template.ParseGlob(TemplateDir + "*.html")
+
+	err := tmpl.ExecuteTemplate(res, "resume", nil)
+
+	if err != nil{
+		fmt.Println("Error executing template :(")
+		fmt.Println(err)
+	}
+}
 
 func main() {
 	fmt.Println("Starting up server...")
 
 	server := http.FileServer(http.Dir(StaticDir))
-	http.Handle("/", server)	//http.StripPrefix("/static/", server))
+	http.Handle("/static/", http.StripPrefix("/static/", server))
+	http.HandleFunc("/", serveSample)
 
 	fmt.Println("Listening on", Port)
-	http.ListenAndServe(":" + Port, nil)
+	err := http.ListenAndServe(":" + Port, nil)
+
+	if err != nil{
+		panic(err)
+	}
 }

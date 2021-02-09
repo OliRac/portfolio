598debf35ad3753274d6acc734bb31dfdbeca823
OliRac <olivieracette@gmail.com>
2021-02-09 11:13:15 -0500

    removed db directory, fixed experience padding
    DB was removed since the files in there were not used. The padding for the experience cards was all wrong because of the footer rule in css. fixed that by adding an id to the page footer.

--- db/queries.sql
+++
@@ -1,6 +0,0 @@
-/*Sample queries*/
-SELECT degree, institution, location, EXTRACT (YEAR FROM grad_year)
-	FROM public.t_education 
-	INNER JOIN public.t_language 
-	ON t_education.lang_id = t_language.id
-	WHERE t_language.code='en';
